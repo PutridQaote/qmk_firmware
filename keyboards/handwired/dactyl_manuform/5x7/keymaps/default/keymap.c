da5c10197e448056bd948c1ca47cb349d5461a92
PutridQaote <m.tyler.haas@gmail.com>
2022-07-26 16:33:37 -0700

    added advanced macros
    got copy path working, and added pitch L and pitch R for remote sessions and working on my home desktop computer

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/default/keymap.c
@@ -9,16 +9,66 @@
 #define _MOUSE 2
 #define _SYMBO 3
 
+
+
 // Some basic macros
 #define TASK   LCTL(LSFT(KC_ESC))
 #define TAB_R  LCTL(KC_TAB)
 #define TAB_L  LCTL(LSFT(KC_TAB))
 #define TAB_RO LCTL(LSFT(KC_T))
 
-#define CPY_PT LALT(KC_P3) // might as well give it a shot eh?
 #define KC_COPY LCTL(KC_C)
 #define KC_CUT LCTL(KC_X)
 #define KC_PASTE LCTL(KC_V)
+#define KC_PREMOT LCTL(LALT(PAUSE))
+
+// defining advanced macros
+enum custom_keycodes {
+    COPY_PTH = SAFE_RANGE,
+    KC_PTCHL,
+    KC_PTCHR,
+};
+
+// details for advanced macros
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case COPY_PTH:
+        if (record->event.pressed) {
+            // when keycode CPY_PT is pressed
+            SEND_STRING(SS_TAP(X_LALT) SS_DELAY(10) SS_TAP(X_3));
+        } else {
+            // when keycode is released
+        }
+        break;
+
+    case KC_PTCHL:
+        if (record->event.pressed) {
+            // when keycode is pressed
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LALT) SS_DOWN(X_PAUSE));
+            SEND_STRING(SS_UP(X_LCTL) SS_UP(X_LALT) SS_UP(X_PAUSE) SS_DELAY(100));
+            //SEND_STRING(SS_TAP(X_LCTL(X_LALT(X_PAUSE))));
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LGUI) SS_DOWN(X_LEFT));
+            SEND_STRING(SS_UP(X_LCTL) SS_UP(X_LGUI) SS_UP(X_LEFT));
+        } else {
+            // when keycode is released
+        }
+        break;
+
+    case KC_PTCHR:
+        if (record->event.pressed) {
+            // when keycode is pressed
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LGUI) SS_DOWN(X_RIGHT));
+            SEND_STRING(SS_UP(X_LCTL) SS_UP(X_LGUI) SS_UP(X_RIGHT) SS_DELAY(100));
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LALT) SS_DOWN(X_PAUSE));
+            SEND_STRING(SS_UP(X_LCTL) SS_UP(X_LALT) SS_UP(X_PAUSE));
+            //SEND_STRING(SS_TAP(X_LCTL(X_LALT(X_PAUSE))));
+        } else {
+            // when keycode is released
+        }
+        break;
+    }
+    return true;
+};
 
 
 // TRACKBALL SUPPORT vvvvvvvvvvvvvvv
@@ -32,20 +82,20 @@ void pointing_device_init_kb(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7(
         // left hand
-        KC_ESC,    KC_1,    KC_2,    KC_3,              KC_4,     KC_5,      KC_DEL,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,              KC_R,     KC_T,      KC_BSPC,
-        KC_HOME,   KC_A,    KC_S,    LT(_MOUSE, KC_D),  KC_F,     KC_G,      KC_ENT,
-        MO(_NUMPAD),KC_Z,   KC_X,    KC_C,              KC_V,     KC_B,      KC_END,
-        KC_GRV,    KC_LGUI, KC_LCTL, KC_LALT,           KC_PAUSE,
+        KC_ESC,     KC_1,    KC_2,    KC_3,              KC_4,     KC_5,      KC_DEL,
+        KC_TAB,     KC_Q,    KC_W,    KC_E,              KC_R,     KC_T,      KC_BSPC,
+        KC_HOME,    KC_A,    KC_S,    LT(_MOUSE, KC_D),  KC_F,     KC_G,      KC_END,
+        MO(_NUMPAD),KC_Z,    KC_X,    KC_C,              KC_V,     KC_B,      KC_ENT,
+        KC_GRV,     KC_LGUI, KC_LCTL, KC_LALT,           KC_PAUSE,
 
                                      KC_SPC,  MO(_SYMBO),
                                      KC_LCTL, KC_LSFT,
                                               MO(_MOUSE), //38 keys including footswitch
         // right hand
-                          KC_MPLY, KC_6,    KC_7,    KC_8,    KC_9,      KC_0,     KC_MINS,
+                          KC_MPLY, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,
                           KC_BSPC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,
-                          KC_ENT,  KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                          KC_HOME, KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_PGUP,
+                          KC_HOME, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+                          KC_ENT,  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_PGUP,
                                             KC_VOLU, KC_VOLD,  KC_EQL,  TG(_NUMPAD),KC_PGDN,
              KC_ENT,     KC_RSFT,
              MO(_MOUSE), KC_RCTL,
@@ -55,18 +105,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMPAD] = LAYOUT_5x7(
         // left hand
         QK_BOOT,   _______,   _______,   _______,   _______,   _______,   _______,
-        _______,   _______,   KC_P7,     KC_P8,     KC_P9,     KC_PAST,   KC_PSLS,
-        _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PPLS,   KC_PMNS,
+        KC_PSLS,   KC_PAST,   KC_P7,     KC_P8,     KC_P9,     _______,   _______,
+        KC_PMNS,   KC_PPLS,   KC_P4,     KC_P5,     KC_P6,     _______,   _______,
         _______,   _______,   KC_P1,     KC_P2,     KC_P3,     KC_TAB,    _______,
         _______,   _______,   KC_P0,    KC_PDOT,    KC_P0,
                                     _______, _______,
                                     _______, _______,
                                              _______,
         // right hand
-                          QK_BOOT,   _______,   _______,   _______,   KC_PMNS,   KC_PPLS,   _______,
-                          _______,   _______,   KC_P7,     KC_P8,     KC_P9,     KC_PAST,   _______,
-                          _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   _______,
-                          _______,   KC_TAB,    KC_P1,     KC_P2,     KC_P3,     KC_PSLS,   _______,
+                          QK_BOOT,   _______,   _______,   _______,   _______,   _______,   _______,
+                          _______,   _______,   KC_P7,     KC_P8,     KC_P9,     KC_PAST,   KC_PSLS,
+                          _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PPLS,   KC_PMNS,
+                          _______,   KC_TAB,    KC_P1,     KC_P2,     KC_P3,     _______,   _______,
                                                 KC_P0,    KC_PDOT,    KC_P0,     KC_TRNS,   KC_TRNS,
              _______, _______,
              _______, _______,
@@ -78,17 +128,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______,   _______,   _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,   KC_WH_U,   _______,   _______,
         _______,   KC_WH_L,   KC_BTN2,   _______,   KC_BTN1,   KC_WH_R,   _______,
-        _______,   KC_WBAK,   KC_WFWD,   _______,   KC_WH_D,   _______,   _______,
+        KC_PTCHL,  KC_WBAK,   KC_WFWD,   _______,   KC_WH_D,   _______,   KC_PTCHR,
         _______,   _______,   _______,   _______,   _______,
                                     _______, _______,
                                     _______, _______,
                                              _______,
         // right hand
                           _______,   _______,   _______,   _______,  _______,   _______,   _______,
-                          _______,   KC_CUT,    KC_COPY,   KC_WH_U, KC_PASTE,   KC_RCTL,   _______,
+                          _______,   KC_CUT,    KC_COPY,   KC_WH_U,  KC_PASTE,  KC_RCTL,   _______,
                           _______,   KC_LWIN,   KC_BTN1,   KC_WH_D,  KC_BTN2,   _______,   KC_ESC,
-                          _______,   CPY_PT,    KC_WBAK,   KC_BTN3,   KC_WFWD,   KC_RSFT,   _______,
-                                                KC_WH_L,   _______,   KC_WH_R,   _______,   _______,
+                          _______,   COPY_PTH,  KC_WBAK,   KC_BTN3,   KC_WFWD,  KC_RSFT,   _______,
+                                                KC_WH_L,   _______,   KC_WH_R,  _______,   _______,
              _______, _______,
              _______, _______,
              _______, _______
